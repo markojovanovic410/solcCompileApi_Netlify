32c31dbc726d7c85492d5c60d899538c4a552d81
markojovanovic410 <markojovfreelancer@gmail.com>
2023-02-25 07:54:46 +0100

    update

--- src/api.js
+++ src/api.js
@@ -2,12 +2,21 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const app = express();
-const bodyParser = require("body-parser");
+const router = express.Router();
+const bodyParser = require('body-parser');
+const solc = require("solc");
+
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(
+  bodyParser.urlencoded({
+    // to support URL-encoded bodies
+    extended: true,
+  })
+);
 
 function solcCompile(data) {
   if (!data.code || data.code == "") {
-    res.json({ result: -1, error: "Need contract code!" });
-    return;
+    return { result: -1, error: "Need contract code!" };
   }
   let eo = true;
   let oa = 200;
@@ -35,32 +44,31 @@ function solcCompile(data) {
 
   const output = JSON.parse(solc.compile(JSON.stringify(input)));
   if (output.contracts) {
-    res.json({
+    return {
       result: 1,
       abi: output.contracts.a.SimpleStorage.abi,
       bytecode: output.contracts.a.SimpleStorage.evm.bytecode.object,
-    });
+    };
   } else {
-    res.json({ result: 0, error: output.errors });
+    return { result: 0, error: output.errors };
   }
   return result;
 }
 
-app.use(bodyParser);
-app.get("/test", (req, res) => {
-    res.json("Test");
-})
-app.get("/", (req, res) => {
-    res.json("aaa");
-})
-app.post("/solc-compile", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+// Define a route that responds with a JSON object when a GET request is made to the root path
+router.get("/", (req, res) => {
+  res.json({
+    hello: "hi!"
+  });
+});
+
+router.post("/solc-compile", (req, res) => {
   const result = solcCompile(req.body);
   res.json(result);
 });
 
+app.use(`/.netlify/functions/api`, router);
+
+// Export the app and the serverless function
+module.exports = app;
 module.exports.handler = serverless(app);
